3ae30b3b07431c2022ceb0f7b8857a3a989b8e23
Dainius <100964434+DainiusMI@users.noreply.github.com>
2022-04-09 21:11:15 +0300

    visual part done

---
+++ index.js
@@ -0,0 +1,6 @@
+
+
+let stOption = document.getElementById('option1');
+let ndOption = document.getElementById('option2');
+let rdOption = document.getElementById('option3');
+let thOption = document.getElementById('option4');
